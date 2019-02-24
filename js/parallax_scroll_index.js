18488c2df86eddad14e78d9239121efa50491dce
Amy Huang <amyhuang.xg@gmail.com>
2019-02-24 15:09:49 -0500

    edit index

--- js/parallax_scroll_index.js
+++ js/parallax_scroll_index.js
@@ -7,57 +7,63 @@ $(document).on("scroll", function(){
   $(".comma").css("top", distanceScrolled*-2);
   $(".first").css("top", distanceScrolled*-2);
   $(".introparagraph").css("right", distanceScrolled*-3.5);
-  $(".behindlink").css("right", distanceScrolled*-2);
-  $(".aboutlink").css("left", distanceScrolled*-1);
-  $(".insightslink").css("right", distanceScrolled*-1);
+  $(".herein").css("left", distanceScrolled*-3.5);
+  $(".aboutlink").css("left", distanceScrolled*-2);
+  $(".contactlink").css("right", distanceScrolled*-2);
 
-  if (distanceScrolled < winW*0.1){
+  if (distanceScrolled < winW*0.07){
     $(".aboutlink").css("top", 0);
-    $(".insightslink").css("top", 0);
+    $(".contactlink").css("top", 0);
   }
 
-  if (distanceScrolled >= winW*0.1){
-    $(".aboutlink").css("top", (distanceScrolled-0.1*winW)*-3);
-    $(".insightslink").css("top", (distanceScrolled-0.1*winW)*-3);
+  if (distanceScrolled >= winW*0.07){
+    $(".aboutlink").css("top", (distanceScrolled-0.07*winW)*-3);
+    $(".contactlink").css("top", (distanceScrolled-0.07*winW)*-3);
   }
 
-  if (distanceScrolled <= winW*0.176){
+  if (distanceScrolled <= winW*0.205){
     $(".name").css("font-size", "6.5vw");
     $(".name").css("letter-spacing", "0.4vw");
     $(".name").css("line-height", "1");
     $(".name").css("position", "relative");
-    $(".name").css("left", distanceScrolled*-1.25);
+    $(".name").css("left", distanceScrolled*-1.05);
     $(".name").css("top", 0);
   }
 
-  if (distanceScrolled > winW*0.176){
-    $(".name").css("font-size", 0.065*winW-(distanceScrolled-0.176*winW)*0.04/0.394);
-    $(".name").css("letter-spacing", 0.004*winW-(distanceScrolled-0.176*winW)*0.0026/0.394);
+  if (distanceScrolled > winW*0.205){
+    $(".name").css("font-size", 0.065*winW-(distanceScrolled-0.205*winW)*0.04/0.305);
+    $(".name").css("letter-spacing", 0.004*winW-(distanceScrolled-0.205*winW)*0.0026/0.305);
     $(".name").css("line-height", "1");
     $(".name").css("position", "absolute");
     $(".name").css("left", "-22vw");
-    $(".name").css("top", distanceScrolled*1-winW*0.176);
+    $(".name").css("top", distanceScrolled*1-winW*0.205);
   }
 
-  if (distanceScrolled >= winW*0.57) {
+  if (distanceScrolled >= winW*0.51) {
     $(".name").css("font-size", "2.5vw");
     $(".name").css("letter-spacing", "0.14vw");
     $(".name").css("line-height", "1");
     $(".name").css("position", "fixed");
-    $(".name").css("top", "2vw");
+    $(".name").css("top", "1.6vw");
     $(".name").css("left", "5vw");
   }
 
-  if (distanceScrolled >= winW*0.57) {
+  if (distanceScrolled >= winW*0.53) {
+  $(".aboutlink").css("left", "-10vw");
+  $(".contactlink").css("right", "-10vw");
+}
+
+  if (distanceScrolled >= winW*0.53) {
     $(".barmenu").css("position", "fixed");
     $(".barmenu").css("left", "21vw");
+    $(".barmenu").css("top", "1.2vw");
   }
   else{
     $(".barmenu").css("position", "relative");
     $(".barmenu").css("left", "15vw");
   }
 
-  if (distanceScrolled >= winW*0.57) {
+  if (distanceScrolled >= winW*0.53) {
     $(".whitebar").css("position", "fixed");
   }
   else{
